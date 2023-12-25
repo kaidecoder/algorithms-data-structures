f2be8921331d4c9d19eb48fbf3d620997972056e
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-12-24 20:44:04 -0500

    binary search

--- index.js
+++ index.js
@@ -332,13 +332,13 @@ console.log(linearSearch([-5, 2, 10, 4, 6], 10))
  * ! Binary Search - pseudocode:  If the array is empty, return -1 as the element cannot be found; if the array has elements, find the middle element in the array.  If target is equal to the middle element, return the middle element index; If target is less than the middle element, binary search left half of the array.  If target is greater than middle element, binary search right half of the array.
 **/
 //2.  Given a sorted array of "n" elements and a target element "t", find the index of "t" in the array.  Return -1 if the target element is not found. (Note: binary search only works on a sorted array)
-//my solution
+//my solution 
 function binarySearch(arr, target){
     for(let i=0; i<arr.length; i++){
         if(arr.length === 0){
             return -1
         }else{
-            let mid = arr.length /2
+            let mid = Math.floor(arr.length /2)
             if(mid === target){
                 return arr.indexOf(target)
             }else if(target < mid){
@@ -356,6 +356,34 @@ function binarySearch(arr, target){
             }
         }
     }
+    return -1
 }
-console.log(binarySearch([-5, 2, 4, 6, 10], 2))
+console.log(binarySearch([-5, 2, 4, 6, 10], 10))
+console.log(binarySearch([-5, 2, 4, 6, 10], 6))
+console.log(binarySearch([-5, 2, 4, 6, 10], 20))
 
+//Time complexity: O(n^3) ???
+
+//Teacher's solution
+function binarySearch2(arr, target){
+    let leftIndex = 0
+    let rightIndex = arr.length - 1
+
+    while(leftIndex <= rightIndex){
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+        if(target === arr[middleIndex]){
+            return middleIndex
+        }
+        if(target < arr[middleIndex]){
+            //One position to the left of middleIndex
+            rightIndex = middleIndex - 1
+        }else{
+            //one position to the right of middleIndex
+            leftIndex = middleIndex + 1
+        }
+    }
+    return -1
+}
+console.log(binarySearch2([-5, 2, 4, 6, 10], 10))
+console.log(binarySearch2([-5, 2, 4, 6, 10], 6))
+console.log(binarySearch2([-5, 2, 4, 6, 10], 20))
